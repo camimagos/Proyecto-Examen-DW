196f17376004aab19c4aa447ed873e7d5fa49900
Camila Joana Magos Fragoso <78517872+camimagos@users.noreply.github.com>
2024-10-15 22:33:16 -0600

    intento de push de solo resultados.php

---
+++ Resultados.php
@@ -0,0 +1,18 @@
+<div>
+    <table>
+        <thead class="table-head">
+            <tr>
+                <th>ID</th>
+                <th>Correo</th>
+                <th>Fecha</th>
+                <th>Duración</th>
+                <th>Categoría</th>
+                <th>Asesor</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+
+    </table>
+</div>
